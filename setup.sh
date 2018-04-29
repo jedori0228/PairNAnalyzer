16c798082a2e31a380f57266758fee60d5a0cc22
Jae Sung Kim <jae.sung.kim@cern.ch>
2018-04-29 21:29:36 +0900

    init

--- setup.sh
+++ setup.sh
@@ -5,8 +5,9 @@ export FILE_PATH=$PLOTTER_WORKING_DIR/rootfiles/
 export PLOT_PATH=/home/jskim/Dropbox/PairN_13TeV/plots/
 export SCRIPT_DIR=$PLOTTER_WORKING_DIR/script/
 export ROOT_INCLUDE_PATH=$ROOT_INCLUDE_PATH:$PLOTTER_WORKING_DIR/include/:$PLOTTER_WORKING_DIR/src/
-export CATVERSION="v8-0-7"
-export CATANVERSION="v8-0-7.37"
+
+export CATVERSION="v944_3"
+export CATANVERSION="v944_3__Default"
 
 alias lqout='cd '$PLOT_PATH
 
